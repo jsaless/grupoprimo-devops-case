eb5e677f027855506129e1bc7071aebfdf7ca09e
jsaless <joaovssales@gmail.com>
2025-02-13 00:32:26 -0300

    chore: config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,59 @@
-import globals from "globals";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import prettier from 'eslint-plugin-prettier';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-/** @type {import('eslint').Linter.Config[]} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+    ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
+    {
+        plugins: {
+            prettier,
+            '@typescript-eslint': typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+
+        settings: {
+            'import/resolver': {
+                typescript: {},
+            },
+        },
+
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    printWidth: 120,
+                    singleQuote: true,
+                    trailingComma: 'es5',
+                    semi: true,
+                    endOfLine: 'auto',
+                    useTabs: false,
+                    tabWidth: 4,
+                },
+            ],
+
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+    },
 ];
